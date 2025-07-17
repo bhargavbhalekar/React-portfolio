f1d80b3517164bfd84920fdaf7506d0e43cb02a9
bhargavbhalekar <bhalekarbhargav12@gmail.com>
2025-07-18 00:32:08 +0530

    initial commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { use, useState } from 'react'
-import './Navbar.css'
+import './navbar.css'
 import logo from '../../assets/logo.svg'; // Correct relative path from /src/components/Navbar/
 import underline from '../../assets/nav_underline.svg'
 import AnchorLink from 'react-anchor-link-smooth-scroll';
